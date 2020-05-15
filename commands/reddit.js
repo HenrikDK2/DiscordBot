360e37db1c35fd71f02aa0cbe8eeb0c8525e1d59
HenrikDK2 <henrik265@outlook.dk>
2020-05-15 23:40:20 +0200

    Post history

--- commands/reddit.js
+++ commands/reddit.js
@@ -16,7 +16,6 @@ module.exports.run = async function (subReddit) {
   await page.goto(subReddit, { waitUntil: "networkidle2" });
   let data = await page.evaluate(() => {
     let post = document.querySelector('div[data-subreddit="FreeGameFindings"]');
-
     let title = post.querySelector(`.title`).textContent;
     let url = post.querySelector(`.thumbnail`).href;
     let thumbnail;
@@ -32,10 +31,27 @@ module.exports.run = async function (subReddit) {
     };
   });
   await browser.close();
-  if (jsonFile === null || data.url !== jsonFile.url) {
-    if (jsonFile !== null)
-      console.log("data: " + data.url + "  jsonFile: " + jsonFile.url);
-    fs.writeFileSync("data.json", JSON.stringify(data));
+  let shouldMerge = true;
+  let newJson = { items: [data] };
+  if (jsonFile !== null) {
+    for (let i = 0; i < jsonFile.items.length; i++) {
+      if (
+        Object.entries(jsonFile.items[i]).toString() ===
+        Object.entries(data).toString()
+      ) {
+        shouldMerge = false;
+        break;
+      }
+    }
+    if (jsonFile.items.length > 7) jsonFile.items.pop();
+
+    if (shouldMerge === true) {
+      newJson = { items: [data].concat(jsonFile.items) };
+    }
+  }
+
+  if (shouldMerge === true) {
+    fs.writeFileSync("data.json", JSON.stringify(newJson));
     return data;
   }
   return null;
