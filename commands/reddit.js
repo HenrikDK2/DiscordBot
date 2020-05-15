b9f450fcec4e7d624ede92e5d291d9ea6c71a6b7
HenrikDK2 <henrik265@outlook.dk>
2020-05-15 16:09:16 +0200

    Compare with title instead of id

--- commands/reddit.js
+++ commands/reddit.js
@@ -28,7 +28,7 @@ module.exports.run = async function (subReddit) {
     };
   });
   await browser.close();
-  if (jsonFile === null || jsonFile.id !== data.id) {
+  if (jsonFile === null || title.id !== title.id) {
     if (jsonFile !== null) console.log(jsonFile, data);
     fs.writeFileSync("data.json", JSON.stringify(data));
     return data;
